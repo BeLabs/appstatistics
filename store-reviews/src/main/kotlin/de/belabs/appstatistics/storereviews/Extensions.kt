93f7b745d21f47b8843e7eedd65f61ffcfe3fb46
Niklas Baudy <niklas.baudy@vanniktech.de>
2024-11-28 13:06:34 +0700

    Technical: Update ktlint to 1.4.1 (#240)

--- store-reviews/src/main/kotlin/de/belabs/appstatistics/storereviews/Extensions.kt
+++ store-reviews/src/main/kotlin/de/belabs/appstatistics/storereviews/Extensions.kt
@@ -1,7 +1,6 @@
 package de.belabs.appstatistics.storereviews
 
-internal fun NotifierReviewFilter?.matches(review: Review) =
-  this == null || languages.orEmpty().any { languageFilter ->
-    review.language.orEmpty().lowercase()
-      .startsWith(languageFilter.lowercase())
-  }
+internal fun NotifierReviewFilter?.matches(review: Review) = this == null || languages.orEmpty().any { languageFilter ->
+  review.language.orEmpty().lowercase()
+    .startsWith(languageFilter.lowercase())
+}
